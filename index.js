07dc5996c5c7ffbda93a57e737f94b36ae3f4f1e
Yanky Hermawan <yankyhermawan42@gmail.com>
2023-02-02 22:10:12 +0700

    Final

---
+++ index.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	const first_name = document.getElementById("first_name").value;
+	const last_name = document.getElementById("last_name").value;
+	document.getElementById("magic").innerHTML = first_name + " " + last_name;
+}
